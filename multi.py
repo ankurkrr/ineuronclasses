9786e980f23b94ae016e3558bd0ec9dc4c9ee426
ANKUR KUMAR <105548142+ankurkrr@users.noreply.github.com>
2022-08-29 10:57:34 +0530

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+
+def multiplicate(a,b):
+    return a*b
+
+def power(a,b):
+    return a**b
---
+++ test7.py
@@ -0,0 +1,12 @@
+data = {
+    "name" : "sudh",
+    "course" : ["ml" , "dl" , "py","stats", "cv"]
+    "msg":"this is my class "
+}
+
+def get_course():
+    return data["course"]
+
+
+def msg():
+    return data["msg"]
